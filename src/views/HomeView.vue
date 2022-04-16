ba4f87b8c0ecd003135858b25fc5fae80df62161
Luciano Amâncio dos Santos <madruga665@hotmail.com>
2022-04-16 11:23:38 -0300

    feature(HomeView): add footer e estilização

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,34 @@
 <template>
-<HeaderComponent />
-  <Card />
+  <main class="container">
+    <HeaderComponent />
+    <Card />
+    <FooterComponent />
+  </main>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from "vue";
-  import HeaderComponent from "../components/HeaderComponent.vue";
-import Card from "@/components/Card.vue";
+import { defineComponent } from "vue";
+import HeaderComponent from "../components/HeaderComponent.vue";
+import Card from "../components/Card.vue";
+import FooterComponent from "../components/FooterComponent.vue";
 
-  export default defineComponent({
-    name: "HomeView",
-    components: {
+export default defineComponent({
+  name: "HomeView",
+  components: {
     HeaderComponent,
-    Card
-},
-  });
+    Card,
+    FooterComponent,
+  },
+});
 </script>
+
+<style scoped>
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    height: 100%;
+  }
+</style>
