1cfb72add3c6da25e8a3d95bfd5dff0f75cf0d9e
Luciano Amâncio dos Santos <madruga665@hotmail.com>
2022-04-16 11:22:48 -0300

    feature(HeaderComponent): remove o box-sizing do style do componente

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -23,7 +23,6 @@ header {
   color: #fff;
   padding: 0 2rem;
   box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
-  box-sizing: border-box;
 }
 img {
   margin-right: 1rem;
